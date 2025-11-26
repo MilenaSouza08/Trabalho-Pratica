6af6e24568943e76b5f118da483b81558a8f7e37
Milena <mile.26souza08@gmail.com>
2025-11-26 08:38:42 -0300

    mudei a senha

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const client = new Client({
     host: 'localhost',             
     database: 'tabelaPratica',  
     password: 'senai',             
-    port: 5432,                    
+    port: 5434,                    
 });
 
 // Conecta ao banco de dados e exibe mensagem de sucesso ou erro
